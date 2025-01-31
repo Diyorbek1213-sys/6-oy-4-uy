28b8c949426d00a70e53065527ef0fbeb907048e
Diyorbek1213-sys <birorbirorkim@gmail.com>
2025-01-31 15:12:40 +0500

    6 oy 4

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
   const [description, setDescription] = useState('')
   const [input, setInput] = useState([])
   const [text, setText] = useState('')
-  const [checkbox_1, setCheckbox_1] = useState('')
   const [isChecked, setIsChecked] = useState(false)
   const buttonn = useRef()
 
@@ -76,15 +75,33 @@ function App() {
   return (
     <>
       {/* 1-vazifa */}
-      <form onSubmit={getData}>
+      <p className='paragraph'> Login Formasi Yaratish
+        Foydalanuvchi login formasi yarating. Formada quyidagi maydonlar bo'lsin:
+        Email
+        Parol
+        Formani yuborishda foydalanuvchi kiritgan qiymatlarni konsolga chiqaring.
+        Foydalanuvchi email maydonini to'ldirmasa, "Email kiritilishi shart!" xabarini chiqaring.
+      </p>
+      <form className='form_1' onSubmit={getData}>
         <input onChange={(e) => { setEmail(e.target.value) }} value={email} type="email" placeholder='enter email...' />
         <input onChange={(e) => { setPassword(e.target.value) }} value={password} type="password" placeholder='enter password...' />
-        <button type='submit'>Jo'natish</button>
+        <br />
+        <br />
+        <button className='button_1' type='submit'>Jo'natish</button>
 
         {/* {error && <p>{error}</p>} */}
       </form> <br /> <br />
       {/* 2-vazifa */}
-      <form onSubmit={getData_2}>
+      <p className='paragraph'>. Ro'yxatdan O'tish Formasi
+        Quyidagi maydonlarga ega forma yarating:
+        Ism
+        Familiya
+        Email
+        Parol
+        Parolni tasdiqlash
+        Foydalanuvchi parol va tasdiqlash parolini bir xil kiritmasa, "Parollar mos emas!" xabarini ko'rsating.
+      </p>
+      <form className='form_2' onSubmit={getData_2}>
         <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='enter name...' />
         <input value={surname} onChange={(e) => { setSurname(e.target.value) }} type="text" placeholder='enter surname...' />
         <input value={email_2} onChange={(e) => { setEmail_2(e.target.value) }} type="email" placeholder='enter email...' />
@@ -93,14 +110,26 @@ function App() {
         <button type='submit'>Jo'natish</button>
       </form> <br /> <br />
       {/* 3-vazifa */}
-      <form onSubmit={getData_3}>
+      <p className='paragraph'>. Kontakt Formasi
+        Quyidagi elementlarga ega kontakt formasi yarating:
+        Ism
+        Email
+        Xabar (textarea)
+        Foydalanuvchi xabar maydoniga kamida 20 ta belgidan iborat matn kiritmasa, "Xabar kamida 20 ta belgidan iborat bo'lishi kerak!" xabarini ko'rsating.
+      </p>
+      <form className='form_3' onSubmit={getData_3}>
         <input value={name_2} onChange={(e) => { setName_2(e.target.value) }} type="text" placeholder='enter name...' />
         <input value={email_3} onChange={(e) => { setEmail_3(e.target.value) }} type="email" placeholder='enter name...' />
         <textarea value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder='description'></textarea>
         <button type='submit'>Jo'natish</button>
       </form>
       {/* 4-vazifa */}
-      <form>
+      <p className='paragraph'>. Dinamik Input Qo'shish
+        Dastlab faqat bitta input maydonidan iborat forma yarating.
+        "Yana input qo'shish" tugmachasi bosilganda yangi input maydoni dinamik ravishda qo'shilsin.
+        Har bir input maydoniga kiritilgan qiymatni forma yuborilganda konsolga chiqaring.
+      </p>
+      <form className='form_4'>
         {input.map((value, index) => (
           <input
             key={index}
@@ -115,7 +144,13 @@ function App() {
         <br /><br />
       </form>
       {/* 5-vazifa */}
-      <form>
+      <p className='paragraph'> Checkbox Bilan Yuborish Tugmasi Aktivlashtirish
+        Kontakt formasini yarating va formaga quyidagi elementni qo'shing:
+        "Men shartlarni qabul qilaman" checkbox elementi
+        Yuborish tugmasi
+        Checkbox belgilangunga qadar yuborish tugmasi deaktiv bo'lsin. Checkbox belgilanganda esa tugma aktiv bo'lsin.
+      </p>
+      <form className='form_5'>
         <label htmlFor="input">Men shartlarni qabul qilaman" checkbox elementi</label>
         <br />
         <input id='input' checked={isChecked} name='checkbox' onChange={() => setIsChecked(!isChecked)} type="checkbox" />
